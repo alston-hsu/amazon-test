7bba761a414758067a618d665153d5a8884acb57
alston-hsu <alston.hsu@outlook.com>
2020-08-07 08:38:22 -0500

    ChromeDriver added to BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,8 @@
 package tests;
 
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.UnreachableBrowserException;
 
@@ -18,15 +18,14 @@ public class BaseTest {
     public void setup() throws MalformedURLException {
 
         try {
-            FirefoxOptions options = new FirefoxOptions();
-            options.setCapability("marionette", true);
+            ChromeOptions options = new ChromeOptions();
             options.setCapability("enableVNC", true);
             options.setCapability("enableVideo", false);
 
             URL remoteUrl = new URL("http://localhost:4444/wd/hub");
             driver = new RemoteWebDriver(remoteUrl, options);
         } catch(UnreachableBrowserException failedConnection) {
-            driver = new FirefoxDriver();
+            driver = new ChromeDriver();
         }
     }
 
